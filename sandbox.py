3ceec2b84fb7609ee11a7ef993602a7a5cf51f53
yjb23tech <yjb23tech@gmail.com>
2023-10-06 02:14:01 +0100

    Adding info

--- sandbox.py
+++ sandbox.py
@@ -3,9 +3,11 @@ from class_tile import Tile
 from funcs import str_get_player_input, view_world_map
 from data_storage import arr_world_map, dict_moves_in_y, dict_moves_in_x, arr_player_input_options 
 
+#Single run only
 test_player = Player("Yuri Orlov", "Odessa", 47)
 print(test_player)
 
+#Everything below Exists inside 'while' game loop until declared otherwise
 view_world_map(arr_world_map)
 
 test_player.player_tile_location(arr_world_map)
