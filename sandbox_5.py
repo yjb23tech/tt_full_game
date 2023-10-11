191c686002d55f0d7f0bb9d80799cbdc3f82b901
yjb23tech <yjb23tech@gmail.com>
2023-10-11 22:06:41 +0100

    Reflexes

--- sandbox_5.py
+++ sandbox_5.py
@@ -16,9 +16,36 @@ arr_tile_quadrants_unconquered = [
 
 ]
 
-print(" ")
-for x in range(len(arr_tile_quadrants_unconquered)):
-    for y in range(len(arr_tile_quadrants_unconquered[x])):
-        print(arr_tile_quadrants_unconquered[x][y], end="")
+def display_world_map(arr_game_grid):
+
     print(" ")
-print(" ")
+    for x in range(len(arr_game_grid)):
+        for y in range(len(arr_game_grid[x])):
+            print(arr_game_grid[x][y], end="")
+        print(" ")
+    print(" ")
+
+bool_game_is_on = True
+
+while (bool_game_is_on == True):
+
+    display_world_map(arr_tile_quadrants_unconquered)
+
+    ui_test_cases = input("Run the tests:\n")
+
+    if ui_test_cases == 'Test 1':
+        arr_tile_quadrants_unconquered[2][4] = 'X'
+        arr_tile_quadrants_unconquered[2][5] = " "
+    elif ui_test_cases == 'Test 2':
+        arr_tile_quadrants_unconquered[2][12] = 'X'
+    elif ui_test_cases == 'Test 3':
+        arr_tile_quadrants_unconquered[2][20] = 'X'
+        arr_tile_quadrants_unconquered[2][21] = " "
+    elif ui_test_cases == 'QUIT':
+        print("Bye now")
+        bool_game_is_on = False
+    else:
+        print("Run again")
+
+
+
